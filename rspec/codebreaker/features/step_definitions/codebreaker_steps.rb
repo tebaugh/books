679bf910bb443cbada576468630ae8b692ebeda2
tebaugh@gmail.com <vagrant@dev.(none)>
2014-05-01 00:34:36 +0900

    Added code to create a Codebreaker object

--- rspec/codebreaker/features/step_definitions/codebreaker_steps.rb
+++ rspec/codebreaker/features/step_definitions/codebreaker_steps.rb
@@ -3,7 +3,7 @@ Given /^I am not yet playing$/ do
 end
 
 When(/^I start a new game$/) do
-  # express the regexp above with the code you wish you had
+  Codebreaker::Game.new.start
 end
 
 Then(/^I should see "(.*?)"$/) do |arg1|
