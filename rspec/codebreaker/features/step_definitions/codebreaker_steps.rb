d349d30a37bdbed3dcfb9e05dc043a42e2651b42
tebaugh@gmail.com <vagrant@dev.(none)>
2014-05-01 00:11:05 +0900

    Initial import

---
+++ rspec/codebreaker/features/step_definitions/codebreaker_steps.rb
@@ -0,0 +1,11 @@
+Given /^I am not yet playing$/ do
+
+end
+
+When(/^I start a new game$/) do
+  # express the regexp above with the code you wish you had
+end
+
+Then(/^I should see "(.*?)"$/) do |arg1|
+  # express the regexp above with the code you wish you had
+end
