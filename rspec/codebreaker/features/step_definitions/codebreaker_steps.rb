0ae519535ceff0d259274c06215ab2da4dc8c481
tebaugh@gmail.com <vagrant@dev.(none)>
2014-05-01 01:45:26 +0900

    Logical error

--- rspec/codebreaker/features/step_definitions/codebreaker_steps.rb
+++ rspec/codebreaker/features/step_definitions/codebreaker_steps.rb
@@ -1,11 +1,27 @@
+class Output
+  def messages
+    @messages ||= [ ]
+  end
+
+  def puts(message)
+    messages << message
+  end
+end
+
+def output
+  @output ||=Output.new
+end
+
 Given /^I am not yet playing$/ do
 
 end
 
 When(/^I start a new game$/) do
-  Codebreaker::Game.new.start
+  output = Output.new
+  game = Codebreaker::Game.new(output)
+  game.start 
 end
 
-Then(/^I should see "(.*?)"$/) do |arg1|
-  # express the regexp above with the code you wish you had
+Then(/^I should see "([^"]*)"$/) do |message|
+  output.messages.should include(message)
 end
