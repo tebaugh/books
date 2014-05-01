632ff30c032d36c4845850d519534a1faaeefccf
tebaugh@gmail.com <vagrant@dev.(none)>
2014-05-01 11:28:37 +0900

    fixed bug where I was incorrectly init the output object

--- rspec/codebreaker/features/step_definitions/codebreaker_steps.rb
+++ rspec/codebreaker/features/step_definitions/codebreaker_steps.rb
@@ -1,6 +1,6 @@
 class Output
   def messages
-    @messages ||= [ ]
+    @messages ||= []
   end
 
   def puts(message)
@@ -17,11 +17,10 @@ Given /^I am not yet playing$/ do
 end
 
 When(/^I start a new game$/) do
-  output = Output.new
   game = Codebreaker::Game.new(output)
   game.start 
 end
 
-Then(/^I should see "([^"]*)"$/) do |message|
+Then /^I should see "([^"]*)"$/ do |message|
   output.messages.should include(message)
 end
