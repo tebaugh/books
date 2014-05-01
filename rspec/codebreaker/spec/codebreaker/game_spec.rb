aa6751002ab397f5f81e16dcfc0c1a1f46dbc912
tebaugh@gmail.com <vagrant@dev.(none)>
2014-05-01 11:37:46 +0900

    setting up to fail enter guess

--- rspec/codebreaker/spec/codebreaker/game_spec.rb
+++ rspec/codebreaker/spec/codebreaker/game_spec.rb
@@ -9,7 +9,12 @@ module Codebreaker
         output.should_receive(:puts).with('Welcome to Codebreaker!')
         game.start
       end
-      it "prompts for the first guess"
+      it "prompts for the first guess" do
+        output = double('output')
+        game=Game.new(output)
+        output.should_receive(:puts).with('Enter guess:')
+        game.start
+      end
     end
   end
 end
