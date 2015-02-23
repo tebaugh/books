829308944ffb4f9b1d65ca795142ce58f9afd8dc
Terry Ebaugh <tebaugh@gmail.com>
2015-02-23 17:25:53 -0500

    Writing testable documentation

--- testing_python/calculate/calculate/calculate.py
+++ testing_python/calculate/calculate/calculate.py
@@ -2,12 +2,25 @@
 
 class Calculate (object):
   def add(self,x,y):
-    if type(x) == int and type(y) ==int:
-      return x + y
-    else:
-      raise TypeError("Invalid type: {} and {}".format(type(x),type(y)))
+     """Takes two integers and adds them together to produce
+     the result.
+
+     >>> c = Calculate()
+     >>> c.add(1,1)
+     2 
+
+     >>> c.add(25,125)
+     150
+     """
+
+     if type(x) == int and type(y) ==int:
+        return x + y
+     else:
+        raise TypeError("Invalid type: {} and {}".format(type(x),type(y)))
 
 if __name__=='__main__': #pragma: no cover
+  import doctest
+  doctest.testmod()
   calc=Calculate()
   result=calc.add(2,2)
   print result
